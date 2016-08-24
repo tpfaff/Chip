b65064fb9db1a16bd7314436b2fcf9fbb43c8eb0
tpfaff <tyler@go-text.me>
2016-08-24 01:14:58 -0700

    1.0

--- app/src/main/java/com/example/tylerpfaff/chip/MainActivity.java
+++ app/src/main/java/com/example/tylerpfaff/chip/MainActivity.java
@@ -1,12 +1,31 @@
 package com.example.tylerpfaff.chip;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.webkit.CookieManager;
+import android.webkit.HttpAuthHandler;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 import java.io.BufferedInputStream;
 
@@ -21,22 +40,93 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
 
-    String referrer = "https://chipotle.com/chiptopia-barcode?barcode=eyJtZW1iZXIiOiJUeWxlciBQZmFmZiIsImltYWdlIjoiaHR0cHM6Ly9jaGlwdG9waWEtYXBpLmNoaXBvdGxlLmNvbS9iYXJjb2RlP3Rva2VuPWJlNmEzMjRiOGZjOTllOTQ4YTAxMzkxMDgwZjgzZjFmZGE4ZjZhZTNmYjU5ZmM3ODFmMTg2ZTliNTkyZjE1OTk5YjUzMjkyMzExY2UyMjJkNDE0NzMwOTVkMjlkNjgyOTIyYjgwYzQ4YWFiODgyNzRkMmFkY2ZmMTBhMDY2NDVkJnNjYWxlPTgiLCJjb2RlIjoiODAyMTEgMTUzNzI1NTgwIn0=";
-    String token = "be6a324b8fc99e948a01391080f83f1fda8f6ae3fb59fc781f186e9b592f15999b53292311ce222d41473095d29d682922b80c48aab88274d2adcff10a06645d";
+    final String referrer = "https://chipotle.com/chiptopia";
+    final String prefsName = "CHIP_PREFS";
     int scale = 8;
+
+    SharedPreferences sharedPreferences;
+
+    WebView webView;
+    ImageView qrView;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        MobileAds.initialize(getApplicationContext(), "ca-app-pub-8977189469228943~2428085784");
+
         setContentView(R.layout.activity_main);
 
-        final ImageView qrView = (ImageView)findViewById(R.id.qr_image_view);
+        AdView mAdView = (AdView) findViewById(R.id.ad_view);
+        AdRequest adRequest = new AdRequest.Builder().addTestDevice("7526E51362799004B14B46B7812549C3").build();
+        mAdView.loadAd(adRequest);
 
+        sharedPreferences = getSharedPreferences(prefsName,MODE_PRIVATE);
+
+
+        qrView = (ImageView)findViewById(R.id.qr_image_view);
+        webView = (WebView)findViewById(R.id.web_view);
+        progressBar = (ProgressBar)findViewById(R.id.progress_bar);
+
+        if(sharedPreferences.getString("token",null) == null){
+            initWebView();
+            getSupportActionBar().setTitle("Login");
+        }else{
+            webView.setVisibility(View.GONE);
+            loadQRCode();
+        }
+
+
+    }
+
+    private void initWebView(){
+
+        CookieManager.getInstance().removeAllCookie();
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(MainActivity.this, "Oh no! " + description, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+                Log.d(TAG,"< "+request.getRequestHeaders());
+                if(request.getUrl().getHost().equals("chiptopia-api.chipotle.com")){
+                    if(request.getUrl().getPath().equals("/barcode")){
+                        if(request.getUrl().getQueryParameterNames().contains("token") && request.getUrl().getQueryParameterNames().contains("scale")){
+                            sharedPreferences.edit().putString("token",request.getUrl().getQueryParameter("token")).commit();
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    webView.setVisibility(View.GONE);
+                                    loadQRCode();
+                                }
+                            });
+                        }
+
+                    }
+                }
+                return super.shouldInterceptRequest(view, request);
+            }
+        });
+
+        webView.loadUrl("https://chipotle.com/chiptopia");
+    }
+
+    private void loadQRCode(){
+
+        getSupportActionBar().setTitle(getResources().getString(R.string.app_name));
+        if(webView.getVisibility() == View.GONE){
+            progressBar.setVisibility(View.VISIBLE);
+        }
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://chiptopia-api.chipotle.com/")
                 .build();
 
         ChipotleService service = retrofit.create(ChipotleService.class);
+
+        String token = sharedPreferences.getString("token",null);
 
         Call<ResponseBody> call = service.getQRCode(referrer,token,scale);
 
@@ -46,12 +136,15 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG,"SUCCESS!");
                 BitmapDrawable drawable = new BitmapDrawable(response.body().byteStream());
                 qrView.setImageDrawable(drawable);
+                progressBar.setVisibility(View.GONE);
             }
 
             @Override
             public void onFailure(Call<ResponseBody> call, Throwable t) {
                 Log.d(TAG,"FAIL!");
+                Toast.makeText(MainActivity.this,t.getLocalizedMessage(),Toast.LENGTH_LONG).show();
             }
         });
     }
+
 }
